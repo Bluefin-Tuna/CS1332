6329ea3af0f27ea3a595f27f9a3c83d2f5390c56
DumbassNamedTuna <50528857+Bluefin-Tuna@users.noreply.github.com>
2023-03-14 00:53:42 -0400

    Finished HW7

--- HW7/AVL.java
+++ HW7/AVL.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -10,9 +11,9 @@ import java.util.NoSuchElementException;
  * @userid tchopra32
  * @GTID 903785867
  *
- * Collaborators: LIST ALL COLLABORATORS YOU WORKED WITH HERE
+ * Collaborators: N/A
  *
- * Resources: LIST ALL NON-COURSE RESOURCES YOU CONSULTED HERE
+ * Resources: N/A
  */
 public class AVL<T extends Comparable<? super T>> {
 
@@ -44,9 +45,13 @@ public class AVL<T extends Comparable<? super T>> {
      *                                            is null
      */
     public AVL(Collection<T> data) {
-        if (data == null) { throw new IllegalArgumentException(); }
+        if (data == null) {
+            throw new IllegalArgumentException("Collection<T> data cannot be null.");
+        }
         for (T d: data) {
-            if (d == null) { throw new IllegalArgumentException(); }
+            if (d == null) {
+                throw new IllegalArgumentException("No element in data can be null.");
+            }
             this.add(d);
         }
     }
@@ -70,64 +75,10 @@ public class AVL<T extends Comparable<? super T>> {
      * @throws java.lang.IllegalArgumentException if data is null
      */
     public void add(T data) {
-        if (data == null) { throw new IllegalArgumentException(); }
+        if (data == null) {
+            throw new IllegalArgumentException("data cannot be null.");
+        }
         this.root = this.add(this.root, data);
-    }
-
-    private AVLNode<T> add(AVLNode<T> n, T d) {
-        if (n == null) {
-            ++this.size;
-            return new AVLNode<T>(d);
-        }
-        if (d.compareTo(n.getData()) < 0) {
-            n.setLeft(this.add(n.getLeft(), d));
-        } else if (d.compareTo(n.getData()) > 0) {
-            n.setRight(this.add(n.getRight(), d));
-        } else {
-            return n;
-        }
-        this.calc(n);
-        return this.balance(n);
-    }
-
-    private void calc(AVLNode<T> n) {
-        int l = n.getLeft().getHeight();
-        int r = n.getRight().getHeight();
-        n.setHeight(1 + Math.max(l, r));
-        n.setBalanceFactor(l - r);
-    }
-
-    private AVLNode<T> balance(AVLNode<T> n) {
-        if (n.getBalanceFactor() >= 2) {
-            if (n.getLeft().getBalanceFactor() <= -1) {
-                n.setLeft(this.rotateLeft(n.getLeft()));
-            }
-            n = this.rotateRight(n)
-        } else if (n.getBalanceFactor() <= -2) {
-            if (n.getRight().getBalanceFactor() >= 1) {
-                n.setRight(this.rotateRight(n.getRight()));
-            }
-            n = this.rotateLeft(n);
-        }
-        return n;
-    }
-
-    private AVLNode<T> rotateLeft(AVLNode<T> n) {
-        AVLNode<T> c = n.getRight();
-        n.setRight(c.getLeft());
-        c.setLeft(n);
-        this.calc(n);
-        this.calc(c);
-        return c;
-    }
-
-    private AVLNode<T> rotateRight(AVLNode<T> n) {
-        AVLNode<T> c = n.getLeft();
-        n.setLeft(c.getRight());
-        c.setRight(n);
-        this.calc(n);
-        this.calc(c);
-        return c;
     }
 
     /**
@@ -158,15 +109,14 @@ public class AVL<T extends Comparable<? super T>> {
      * @throws java.util.NoSuchElementException   if the data is not found
      */
     public T remove(T data) {
-        if (data == null) { throw new IllegalArgumentException(); }
+        if (data == null) {
+            throw new IllegalArgumentException("data cannot be null.");
+        }
         AVLNode<T> r = new AVLNode<T>(null);
-        this.root = this.remo
+        this.root = this.remove(this.root, r, data);
+        return r.getData();
     }
 
-    private AVLNode<T> remove(AVLNode<T> n, AVLNode<T> r, T d) {
-        if (n == null) { throw new NoSuchElementException(); }
-        if (d.)
-    }
     /**
      * Returns the element from the tree matching the given parameter.
      *
@@ -181,19 +131,10 @@ public class AVL<T extends Comparable<? super T>> {
      * @throws java.util.NoSuchElementException   if the data is not in the tree
      */
     public T get(T data) {
-        if (data == null) { throw new IllegalArgumentException(); }
-        return this.get(this.root, data);
-    }
-
-    private T get(AVLNode<T> n, T d) {
-        if (n == null) { throw new NoSuchElementException(); }
-        if (d.compareTo(n.getData()) < 0) {
-            return this.get(n.getLeft(), d);
-        } else if (d.compareTo(n.getData()) > 0) {
-            return this.get(n.getRight(), d);
-        } else {
-            return d;
+        if (data == null) {
+            throw new IllegalArgumentException("data cannot be null.");
         }
+        return this.get(this.root, data);
     }
 
     /**
@@ -223,9 +164,9 @@ public class AVL<T extends Comparable<? super T>> {
      * @return the height of the root of the tree, -1 if the tree is empty
      */
     public int height() {
-        return this.root == null ? -1: 1 + Math.max(
-            this.root.getLeft() == null ? -1: this.root.getLeft().getHeight(),
-            this.root.getRight() == null ? -1: this.root.getRight().getHeight()
+        return this.root == null ? -1 : 1 + Math.max(
+            this.root.getLeft() == null ? -1 : this.root.getLeft().getHeight(),
+            this.root.getRight() == null ? -1 : this.root.getRight().getHeight()
         );
     }
 
@@ -275,7 +216,9 @@ public class AVL<T extends Comparable<? super T>> {
      * traversal order
      */
     public List<T> deepestBranches() {
-
+        List<T> l = new ArrayList<T>(this.size);
+        this.deepestBranches(this.root, l);
+        return l;
     }
 
     /**
@@ -310,7 +253,15 @@ public class AVL<T extends Comparable<? super T>> {
      * or if data1 > data2
      */
     public List<T> sortedInBetween(T data1, T data2) {
-
+        if (data1 == null || data2 == null) {
+            throw new IllegalArgumentException("data1 or data2 cannot be null.");
+        }
+        if (data1.compareTo(data2) > 0) {
+            throw new IllegalArgumentException("data1 cannot be greater than data2.");
+        }
+        List<T> l = new ArrayList<T>(this.size);
+        this.sortedInBetween(l, this.root, data1, data2);
+        return l;
     }
 
     /**
@@ -337,5 +288,204 @@ public class AVL<T extends Comparable<? super T>> {
     public int size() {
         // DO NOT MODIFY THIS METHOD!
         return size;
+    }
+
+
+    /**
+     * Helper method for public add method.
+     *
+     * @param n AVLNode to recurse on
+     * @param d T data representing the data to add
+     * @return AVLNode representing the balanced AVLNode with the data added
+     */
+    private AVLNode<T> add(AVLNode<T> n, T d) {
+        if (n == null) {
+            ++this.size;
+            return new AVLNode<T>(d);
+        }
+        if (d.compareTo(n.getData()) < 0) {
+            n.setLeft(this.add(n.getLeft(), d));
+        } else if (d.compareTo(n.getData()) > 0) {
+            n.setRight(this.add(n.getRight(), d));
+        } else {
+            return n;
+        }
+        this.calc(n);
+        return this.balance(n);
+    }
+
+    /**
+     * Helper method for findingPredecessor in the remove case with 2 children.
+     *
+     * @param n AVLNode to recurse
+     * @param c AVLNode representing the child
+     * @return AVLNode representing the balanced node
+     */
+    private AVLNode<T> findPredecessor(AVLNode<T> n, AVLNode<T> c) {
+        if (n.getRight() == null) {
+            c.setData(n.getData());
+            return n.getLeft();
+        }
+        n.setRight(this.findPredecessor(n.getRight(), c));
+        this.calc(n);
+        return this.balance(n);
+    }
+
+    /**
+     * Helper method for public remove method.
+     *
+     * @param n AVLNode to recurse on
+     * @param r AVLNode which stores removed data to return in public method
+     * @param d T data to remove
+     * @return AVLNode that has been balanced after data removal
+     */
+    private AVLNode<T> remove(AVLNode<T> n, AVLNode<T> r, T d) {
+        if (n == null) {
+            throw new NoSuchElementException("data is not present in AVL.");
+        }
+        if (d.compareTo(n.getData()) < 0) {
+            n.setLeft(this.remove(n.getLeft(), r, d));
+        } else if (d.compareTo(n.getData()) > 0) {
+            n.setRight(this.remove(n.getRight(), r, d));
+        } else {
+            r.setData(n.getData());
+            --this.size;
+            if (n.getLeft() == null) {
+                return n.getRight();
+            } else if (n.getRight() == null) {
+                return n.getLeft();
+            } else {
+                AVLNode<T> c = new AVLNode<T>(null);
+                n.setLeft(this.findPredecessor(n.getLeft(), c));
+                n.setData(c.getData());
+            }
+        }
+        this.calc(n);
+        return this.balance(n);
+    }
+
+    /**
+     * Helper method for public get method.
+     *
+     * @param n AVLNode to recurse on
+     * @param d T data to get
+     * @return T data retrieved from AVLNode n
+     */
+    private T get(AVLNode<T> n, T d) {
+        if (n == null) {
+            throw new NoSuchElementException("data is not present in AVL.");
+        }
+        if (d.compareTo(n.getData()) < 0) {
+            return this.get(n.getLeft(), d);
+        } else if (d.compareTo(n.getData()) > 0) {
+            return this.get(n.getRight(), d);
+        } else {
+            return d;
+        }
+    }
+
+    /**
+     * Helper method for public deepestBranches method.
+     *
+     * @param n AVLNode to recurse on
+     * @param l List to add data of deepest branches to
+     */
+    private void deepestBranches(AVLNode<T> n, List<T> l) {
+        if (n == null) {
+            return;
+        }
+        l.add(n.getData());
+        if (n.getLeft() != null && n.getHeight() - n.getLeft().getHeight() <= 1) {
+            this.deepestBranches(n.getLeft(), l);
+        }
+        if (n.getRight() != null && n.getHeight() - n.getRight().getHeight() <= 1) {
+            this.deepestBranches(n.getRight(), l);
+        }
+    }
+
+    /**
+     * Helper method for public sortedInBetween method.
+     *
+     * @param l List to add data if said data > data1 and data < data2
+     * @param n AVLNode to recurse on
+     * @param d1 T data1 to compare AVLNode n's data with
+     * @param d2 T data2 to compare AVLNode n's data with
+     */
+    private void sortedInBetween(List<T> l, AVLNode<T> n, T d1, T d2) {
+        if (n == null) {
+            return;
+        }
+        if (d1.compareTo(n.getData()) < 0) {
+            this.sortedInBetween(l, n.getLeft(), d1, d2);
+        }
+        if (d1.compareTo(n.getData()) < 0 && d2.compareTo(n.getData()) > 0) {
+            l.add(n.getData());
+        }
+        if (d2.compareTo(n.getData()) > 0) {
+            this.sortedInBetween(l, n.getRight(), d1, d2);
+        }
+    }
+
+    /**
+     * Helper method for updating height and balanceFactor of node.
+     * 
+     * @param n AVLNode to update heights and balanceFactor for
+     */
+    private void calc(AVLNode<T> n) {
+        int l = n.getLeft() != null ? n.getLeft().getHeight() : -1;
+        int r = n.getRight() != null ? n.getRight().getHeight() : -1;
+        n.setHeight(1 + Math.max(l, r));
+        n.setBalanceFactor(l - r);
+    }
+
+    /**
+     * Helper method for balancing node.
+     *
+     * @param n AVLNode to balance
+     * @return AVLNode n after being balanced
+     */
+    private AVLNode<T> balance(AVLNode<T> n) {
+        if (n.getBalanceFactor() >= 2) {
+            if (n.getLeft().getBalanceFactor() <= -1) {
+                n.setLeft(this.rotateLeft(n.getLeft()));
+            }
+            n = this.rotateRight(n);
+        } else if (n.getBalanceFactor() <= -2) {
+            if (n.getRight().getBalanceFactor() >= 1) {
+                n.setRight(this.rotateRight(n.getRight()));
+            }
+            n = this.rotateLeft(n);
+        }
+        return n;
+    }
+
+    /**
+     * Helper method for performing rotate left balancing operation.
+     *
+     * @param n AVLNode to perform rotate left on
+     * @return AVLNode with rotate left balancing operation performed
+     */
+    private AVLNode<T> rotateLeft(AVLNode<T> n) {
+        AVLNode<T> c = n.getRight();
+        n.setRight(c.getLeft());
+        c.setLeft(n);
+        this.calc(n);
+        this.calc(c);
+        return c;
+    }
+
+    /**
+     * Helper method for performing rotate right balancing operation.
+     *
+     * @param n AVLNode to perform rotate right on
+     * @return AVLNode with rotate right balancing operation performed
+     */
+    private AVLNode<T> rotateRight(AVLNode<T> n) {
+        AVLNode<T> c = n.getLeft();
+        n.setLeft(c.getRight());
+        c.setRight(n);
+        this.calc(n);
+        this.calc(c);
+        return c;
     }
 }
